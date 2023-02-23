8d357d2e24924bc1b6b6a6735732559c9392aae3
Vincent Fuertez <vfuertez@gmail.com>
2023-02-22 23:54:55 -0500

    added models

--- project4backend/project4api/models.py
+++ project4backend/project4api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Info(models.Model):
+    name = models.CharField(max_length=130)
+    link1 = models.CharField(max_length=130)
+    link2 = models.CharField(max_length=130)
+    web = models.CharField(max_length=130)
+    
--- project4backend/project4backend/settings.py
+++ project4backend/project4backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project4api.apps.Project4ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +77,10 @@ WSGI_APPLICATION = 'project4backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
