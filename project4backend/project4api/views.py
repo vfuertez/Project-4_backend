fc03c83f5b1a188d1e1156d4dcba46d99d2d2dcc
Vincent Fuertez <vfuertez@gmail.com>
2023-02-23 01:58:30 -0500

    added viewset

--- project4backend/project4api/views.py
+++ project4backend/project4api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Info
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import InfoSerializer
 
 # Create your views here.
+
+class InfoViewSet(viewsets.ModelViewSet):
+    queryset = Info.objects.all()
+    serializer_class = InfoSerializer
+    permission_classes = [permissions.AllowAny]
