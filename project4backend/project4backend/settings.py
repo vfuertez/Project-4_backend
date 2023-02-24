390ce04bb0428bc6c95423af1ec4b2cf2e78ceb4
Vincent Fuertez <vfuertez@gmail.com>
2023-02-23 22:08:36 -0500

    added setup.sh

--- project4backend/project4backend/settings.py
+++ project4backend/project4backend/settings.py
@@ -29,6 +29,10 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+## Handling Allowed Hosts on Render
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
